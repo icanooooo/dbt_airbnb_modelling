69d57be874cc44f1d29fe8c621d149dd01350dc3
Muhammad Ihsan <muhihsan0@outlook.com>
2024-11-10 23:11:01 +0700

    succesfully created model using dbt through dags airflow

---
+++ airflow/dags/dbt_models.py
@@ -0,0 +1,17 @@
+from airflow import DAG
+from datetime import datetime
+from airflow.operators.bash import BashOperator
+
+with DAG('dbt_models',
+         start_date=datetime(2024, 11, 8),
+         description='Using dbt to create models',
+         tags=['airbnb_euskadi'],
+         schedule='@weekly',
+         catchup=False) as dag:
+    
+    dimensional_dbt = BashOperator(
+        task_id='run_dbt',
+        bash_command='dbt run --models neighbourhoods_table_dim --profiles-dir /opt/airflow/profiles --project-dir /opt/airflow/airbnb_euskadi'
+    )
+
+    dimensional_dbt
