48b987aea62a69b65e2e21921af87addafc3d9eb
Muhammad Ihsan <muhihsan0@outlook.com>
2024-11-11 18:33:06 +0700

    added new dbt models to dags

--- airflow/dags/dbt_models.py
+++ airflow/dags/dbt_models.py
@@ -2,6 +2,10 @@ from airflow import DAG
 from datetime import datetime
 from airflow.operators.bash import BashOperator # type: ignore
 
+dbt_tail = " --profiles-dir /opt/airflow/profiles --project-dir /opt/airflow/airbnb_euskadi"
+dbt_1 = "dbt run --models neighbourhoods_table_dim listing_table-dims review_table_facts" + dbt_tail
+dbt_2 = "dbt run --models host_table_dims" + dbt_tail
+
 with DAG('dbt_models',
          start_date=datetime(2024, 11, 8),
          description='Using dbt to create models',
@@ -9,9 +13,15 @@ with DAG('dbt_models',
          schedule='@weekly',
          catchup=False) as dag:
     
-    dimensional_dbt = BashOperator(
-        task_id='run_dbt',
-        bash_command='dbt run --models neighbourhoods_table_dim listing_table-dims review_table_facts --profiles-dir /opt/airflow/profiles --project-dir /opt/airflow/airbnb_euskadi'
+    cleansing_dbt = BashOperator(
+        task_id='cleansing_tables',
+        bash_command=dbt_1
     )
 
-    dimensional_dbt
+    new_model_dbt = BashOperator(
+        task_id='creating models based on cleansed table',
+        bash_command=dbt_2
+    )
+
+
+    cleansing_dbt >> new_model_dbt
